0d62ff301cd67f12f74dd097a41f2ba552048a57
Gustav <sir.gustav.the.coder@gmail.com>
2024-05-31 20:49:50 +0200

    Ported generator

---
+++ bsdoc/main.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var gen = new Bitsquid.Generator();
+gen.Add("Header", "h1");
+gen.Add("One item", "ul", "li", "p");
+gen.Add(null, "ul");
+gen.Add("Second item", "ul", "li", "p");
+gen.Add("with two lines", "ul", "li", "p");
+Console.WriteLine(gen.Generate());
+
+internal static class Extensions
+{
+    public static IEnumerable<(T value, int i)> each_with_index<T>(this IEnumerable<T> model)
+    {
+        return model.Select((value, i) => (value, i));
+    }
+
+    public static T pop<T>(this List<T> list)
+    {
+        var last = list.Count - 1;
+        var end = list[last];
+        list.RemoveAt(last);
+        return end;
+    }
+}
---
+++ test/generator.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using System.ComponentModel;
+using Xunit;
+using Bitsquid;
+
+namespace test;
+
+public class GeneratorTest
+{
+    [Fact]
+    public void Empty()
+    {
+        var gen = new Generator();
+        gen.Generate().Should().Be("");
+    }
+
+    [Fact]
+    public void SingleHeader()
+    {
+        var gen = new Generator();
+        gen.Add("Header", "h1");
+        gen.Generate().Should().Be("<h1>Header</h1>\n");
+    }
+
+    [Fact]
+    public void Sample()
+    {
+        var gen = new Generator();
+        gen.Add("Header", "h1");
+        gen.Add("One item", "ul", "li", "p");
+        gen.Add(null, "ul");
+        gen.Add("Second item", "ul", "li", "p");
+        gen.Add("with two lines", "ul", "li", "p");
+        gen.Generate().Should().Be(
+            "<h1>Header</h1>\n" +
+            "\n" +
+            "<ul>\n" +
+            "\t<li><p>One item</p></li>\n" +
+            "\t<li>\n" +
+            "\t\t<p>\n" +
+            "\t\t\tSecond item\n" +
+            "\t\t\twith two lines\n" +
+            "\t\t</p>\n" +
+            "\t</li>\n" +
+            "</ul>\n"
+        );
+    }
+
+    [Fact]
+    public void SampleWith2Null()
+    {
+        var gen = new Generator();
+        gen.Add("Header", "h1");
+        gen.Add("One item", "ul", "li", "p");
+        gen.Add(null, "ul");
+        gen.Add(null, "ul");
+        gen.Add("Second item", "ul", "li", "p");
+        gen.Add("with two lines", "ul", "li", "p");
+        gen.Generate().Should().Be(
+            "<h1>Header</h1>\n" +
+            "\n" +
+            "<ul>\n" +
+            "\t<li><p>One item</p></li>\n" +
+            "\t<li>\n" +
+            "\t\t<p>\n" +
+            "\t\t\tSecond item\n" +
+            "\t\t\twith two lines\n" +
+            "\t\t</p>\n" +
+            "\t</li>\n" +
+            "</ul>\n"
+        );
+    }
+}
